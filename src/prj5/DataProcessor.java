4b44e59524349aac8a9c720cf01b6aab54a965d9
khang1999 <38816851+khang1999@users.noreply.github.com>
2018-11-12 02:28:25 -0500

    DataProcessor/Glyph

--- src/prj5/DataProcessor.java
+++ src/prj5/DataProcessor.java
@@ -7,5 +7,28 @@
 package prj5;
 
 public class DataProcessor {
-
+    private Window window;
+    private LinkedList<Person> people;
+    private LinkedList<Song> songs;
+    private LinkedList<Glyph> glyphs;
+    
+    public DataProcessor(Window w) {
+        window = w;
+    }
+    
+    /**
+     * Generates the glyphs
+     */
+    public void generateGlyphs() {
+        
+    }
+    
+    /**
+     * Calculates votes based on index of song
+     * @param s song to check for
+     * @return song index and votes
+     */
+    public double[][] calculateVotes(Song s){
+        //run through each people at a specific vote
+    }
 }
--- src/prj5/Glyph.java
+++ src/prj5/Glyph.java
@@ -14,5 +14,20 @@ public class Glyph {
     private double[] regions;
     private double[] hobbies;
     
+    public Glyph(Window w, Song s, double[] m, double[] r, double[] h) {
+        window = w;
+        song = s;
+        majors = m;
+        regions = r;
+        hobbies = h;
+    }
     
+    /**
+     * Adds a glyph to window
+     * @param x x value of glyph
+     * @param y y value of glyph
+     */
+    public void addToWindow(int x, int y) {
+        
+    }
 }
