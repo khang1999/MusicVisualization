eee5a743d1a9d11495467a0696f417b8d3ef3053
khang1999 <38816851+khang1999@users.noreply.github.com>
2018-11-15 20:47:31 -0500

    GlyphTest

--- src/prj5/GlyphTest.java
+++ src/prj5/GlyphTest.java
@@ -8,5 +8,12 @@ package prj5;
  *
  */
 public class GlyphTest {
-
+    private Glyph glyph;
+    private Song song;
+    
+    public void setUp() {
+        song = new Song("Love Story", "Taylor Swift", "love", 2002);
+        
+        glyph = new Glyph();
+    }
 }
