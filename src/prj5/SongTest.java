ba1f4658b19a491b6a2ca2c1fade5bd0cbd4f489
Jason Hassold <JasonHassold@gmail.com>
2018-11-15 23:21:33 -0500

    stuff

--- src/prj5/SongTest.java
+++ src/prj5/SongTest.java
@@ -59,7 +59,8 @@ public class SongTest extends TestCase {
      */
     public void testGetHobbyData() {
         song.setHobbyData(new int[][] {{1}, {2}, {3}});
-        assertTrue(Arrays.deepEquals(new int[][] {{1}, {2}, {3}}, song.getHobbyData()));
+        assertTrue(Arrays.deepEquals(new int[][] {{1}, {2}, {3}}, 
+            song.getHobbyData()));
     }
     
     /**
@@ -67,7 +68,8 @@ public class SongTest extends TestCase {
      */
     public void testGetMajorData() {
         song.setMajorData(new int[][] {{1}, {2}, {3}});
-        assertTrue(Arrays.deepEquals(new int[][] {{1}, {2}, {3}}, song.getMajorData()));
+        assertTrue(Arrays.deepEquals(new int[][] {{1}, {2}, {3}}, 
+            song.getMajorData()));
     }
     
     /**
@@ -75,6 +77,18 @@ public class SongTest extends TestCase {
      */
     public void testGetRegionData() {
         song.setRegionData(new int[][] {{1}, {2}, {3}});
-        assertTrue(Arrays.deepEquals(new int[][] {{1}, {2}, {3}}, song.getRegionData()));
+        assertTrue(Arrays.deepEquals(new int[][] {{1}, {2}, {3}}, 
+            song.getRegionData()));
+    }
+    
+    /**
+     * Test toString
+     */
+    public void testToString() {
+        song.setHobbyData(new int[][] {{0,1,2},{3,4,5},{6,7,8},{9,10,11}});
+        
+        assertEquals("Song Title: Hello\nSong Artist: Khang\nSong Genre: Sad\n" 
+        + "Song Year: 1980\nHeard\nreading:6 art:0 sports:9 music:3\n" +
+          "Likes\nreading:7 art:1 sports:10 music:4\n", song.toString("hobby"));
     }
 }
